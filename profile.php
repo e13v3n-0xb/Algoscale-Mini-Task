905c213a4c3fdca8bc35849e4cde8216da8c9ace
Anushka Soni <36234942+e13v3n-0xb@users.noreply.github.com>
2020-04-20 20:22:52 +0530

    completed profile.php

--- profile.php
+++ profile.php
@@ -1,12 +1,83 @@
-<?php 
-include_once 'connection.php'
-?>
+
+<!DOCTYPE html>
 <html>
+
 <head>
-    <title>Admin Dashboard</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>ADMIN DASHBOARD</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="style5.css">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
 </head>
+
 <body>
-<form method="POST" action="delete.php">
+
+    <div class="wrapper">
+        <!-- Sidebar Holder -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>ADMIN PANEL</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>CONTROLS</p>
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">DASHBOARD</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                         
+                         <li>
+                            <a href="http://127.0.0.1:8080/algoscale/profile.php">CHANGE PASSWORD</a>
+                        </li>
+                        <li>
+                            <a href="http://127.0.0.1:8080/algoscale/profile.php">DELETE USER</a>
+                        </li>
+                    </ul>
+                </li>
+                
+                
+            </ul>
+
+            
+        </nav>			
+           <!-- Page Content Holder -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="navbar-btn">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="logout.php">LOGOUT</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            
+            <h2>WELCOME TO ADMIN PANEL</h2>
+            <p>
+            <form method="POST" action="delete.php">
 <?php
 session_start();
 include 'connection.php';
@@ -42,9 +113,37 @@ $username = $_SESSION['user_name'];
     echo "</select>";
 ?>
 
-<input name="delete" type="submit"  value="Delete">
+<input name="delete" type="submit" onclick = 'return DeleteRecord()'  value="Delete">
 </form>
-<a href="logout.php">Logout</a> 
-</body>
-</html>
+        </p>
 
+            
+
+            </div>
+    </div>
+
+    <script>   
+   function DeleteRecord()   
+   {   
+    return confirm("Do u want to delete");   
+   }   
+  </script>   
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+                $(this).toggleClass('active');
+            });
+        });
+    </script>
+</body>
+
+</html>
